484e49e8ff8e27965f19ef41c7aba816b3f65c25
nahimr <nahim.rouidjali@gmail.com>
2023-06-05 20:55:53 +0200

    Removing foreach in Build.bat

--- .appveyor/Build.bat
+++ .appveyor/Build.bat
@@ -4,8 +4,6 @@ cd %APPVEYOR_BUILD_FOLDER%\.build\build
 
 cmake .. -DBUILD_STATIC=ON -G "Visual Studio 17 2022" -A Win32
 
-for %%c in (Release Debug RelWithDebInfo) do (
-    msbuild .\PACKAGE.vcxproj /p:Configuration="%%c" /p:Platform=Win32 /m ^
-        /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" ^
-        /nologo /ConsoleLoggerParameters:NoSummary;Verbosity=quiet
-)
+msbuild .\PACKAGE.vcxproj /p:Configuration="Release;Debug;RelWithDebInfo" /p:Platform=Win32 /m ^
+    /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" ^
+    /nologo /ConsoleLoggerParameters:NoSummary;Verbosity=quiet
