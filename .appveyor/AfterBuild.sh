aebe19767b2ff0ebf3ba0331ca7e89cf477755e8
nahimr <nahim.rouidjali@gmail.com>
2023-06-05 15:48:59 +0200

    Change CPack Generator for Linux TGZ, changing appveyor build scripts

--- .appveyor/AfterBuild.sh
+++ .appveyor/AfterBuild.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
-for config in debug release release_dev
+cd "$APPVEYOR_BUILD_FOLDER/.build/build"
+
+for config in Release Debug RelWithDebInfo
 do
-    cd "${APPVEYOR_BUILD_FOLDER}/src/bin/${config^}"
-    cp -R ${APPVEYOR_BUILD_FOLDER}/data/* ./
-    tar -czf "REDRIVER2_${config^}.tar.gz" *
+    make package
 done
--- .appveyor/Install.sh
+++ .appveyor/Install.sh
@@ -3,13 +3,8 @@ set -ex
 
 cd "$APPVEYOR_BUILD_FOLDER/src"
 
-# Download premake5
-# because it isn't in the repos (yet?)
-curl "$linux_premake_url" -Lo premake5.tar.gz
-tar xvf premake5.tar.gz
-
 sudo apt-get update -qq -y
-sudo apt-get install --no-install-recommends -y gcc-multilib g++-multilib
+sudo apt-get install --no-install-recommends -y gcc-multilib g++-multilib cmake
 sudo apt-get install -qq aptitude -y
 
 # fix Ubuntu's broken mess of packages using aptitude
